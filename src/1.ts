a90e9ab2ba139e0527a4087c9daee9c7138a0e11
ex1kenny <ex1kenny4543@gmail.com>
2024-04-09 21:04:31 +0300

    home-work3

--- src/1.ts
+++ src/1.ts
@@ -3,17 +3,14 @@
   Замість того, щоб оголошувати ці властивості в тілі класу, потім у конструкторі, і нарешті надавати їм значення, 
   напишіть скорочену ініціалізацію.
 */
+interface StudentClass {
+  name: string;
+  age: number;
+  grade: string;
+}
 
-class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
-
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
+class Student implements StudentClass {
+  constructor(public name: string, public age: number, public grade: string) {}
 }
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,66 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean = false;
+  key: Key;
+  tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  entering(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Opened");
+    } else {
+      console.log("Closed");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door open");
+    } else {
+      console.log("Door close");
+    }
+  }
+}
+
+// Тестування
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
-house.comeIn(person);
-
+house.entering(person);
 
 export {};
